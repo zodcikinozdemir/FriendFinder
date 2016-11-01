acc797d58139816309d8b7f9adc502561234428c
zodcikin <zeynep.odcikin@gmail.com>
2016-10-31 22:10:14 -0400

    Node Express first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv.port || 3000; //--port <port#> or default is 3000
+app.set('port', port);
+
+var server = app.listen(port, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
